bce515d7d4b2020cc3d0ef4399916c83d9d4f963
oanhduong <thanhoanh.se@gmail.com>
2019-11-10 11:59:49 +0700

    Call to Armada Consensus and HH successfully

--- js/exp/example.js
+++ js/exp/example.js
@@ -1,12 +1,12 @@
 //Requires Armada Libary with key
-const armada = require('open-waters')('secretkey');
+const armada = require('open-waters')('XriiU6sm19UUCJwm15ky');
 
 async function test() {
 	// Send data to Hedera Hashgraph
 	let result
 	try {
 		result = await armada.post.create({
-			activityId: 1234,
+			activityId: 123,
 			data: '{lat: 37.7749, long: 122.4194, id: 12}',
 			memo: 'checking '
 		})
