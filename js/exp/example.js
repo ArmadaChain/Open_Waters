971dfb4c6a8738e1d6fd329e95d7399e6379b900
oanhduong <thanhoanh.se@gmail.com>
2019-11-10 11:30:09 +0700

    Fix bug

--- js/exp/example.js
+++ js/exp/example.js
@@ -1,10 +1,20 @@
 //Requires Armada Libary with key
-const armada = require('./open-waters')('secretkey');
+const armada = require('open-waters')('secretkey');
 
-// Send data to Hedera Hashgraph
-const post = armada.post.create({
-	activityID: 1234,
-	data: '{lat: 37.7749, long: 122.4194, id: 12}',
-	memo: 'checking '
+async function test() {
+	// Send data to Hedera Hashgraph
+	let result
+	try {
+		result = await armada.post.create({
+			activityID: 1234,
+			data: '{lat: 37.7749, long: 122.4194, id: 12}',
+			memo: 'checking '
+		})
+	} catch (error) {
+		result = error
+	}
 
-});
+	console.log(result)
+}
+
+test()
--- js/package/index.js
+++ js/package/index.js
@@ -1,28 +1,9 @@
 module.exports = (apikey) => {
-  const http = require('http')
-  const requestOpt = {
-    hostname: 'localhost',
-    port: 8080,
-    path: `data?apikey=${apikey}`,
-    headers: {'Content-Type': 'application/json'},
-  }
+  const axios = require('axios')
   return {
     post: {
-      create: (data = {}) => new Promise((resolve, reject) => {
-        requestOpt.method = 'POST'
-        let req = http.request(requestOpt, (res) => res.on('data', (d) => resolve(d)))
-        req.on('error', reject)
-        req.write(JSON.stringify(data))
-        req.end()
-      }),
-
-      get: (queries) => new Promise((resolve, reject) => {
-        requestOpt.method = 'GET'
-        requestOpt.path += `&${Object.keys(queries).map((key) => `${key}=${queries[key]}`).join('&')}`
-        let req = http.request(requestOpt, (res) => res.on('data', (d) => resolve(d)))
-        req.on('error', reject)
-        req.end()
-      }),
+      create: (data = {}) => axios.post(`http://localhost:8080/data?apikey=${apikey}`, data),
+      get: (queries) => axios.post(`http://localhost:8080/data?apikey=${apikey}&${Object.keys(queries).map((key) => `${key}=${queries[key]}`).join('&')}`, data),
     }
   }
 }
