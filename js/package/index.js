1d3d71d3a65d10c5bafda7a4ad5731e07e519164
oanhduong <thanhoanh.se@gmail.com>
2019-11-11 08:02:31 +0700

    Change the server

--- js/package/index.js
+++ js/package/index.js
@@ -2,8 +2,8 @@ module.exports = (apikey) => {
   const axios = require('axios')
   return {
     post: {
-      create: (data = {}) => axios.post(`http://localhost:8080/data?apikey=${apikey}`, data),
-      get: (queries) => axios.post(`http://localhost:8080/data?apikey=${apikey}&${Object.keys(queries).map((key) => `${key}=${queries[key]}`).join('&')}`, data),
+      create: (data = {}) => axios.post(`http://18.219.198.133:8080/data?apikey=${apikey}`, data),
+      get: (queries) => axios.post(`http://18.219.198.133:8080/data?apikey=${apikey}&${Object.keys(queries).map((key) => `${key}=${queries[key]}`).join('&')}`, data),
     }
   }
 }
