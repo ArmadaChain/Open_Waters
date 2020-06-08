abade0f26699df629e95e1a735947082c945e2b2
dthanhoanh <dthanhoanh@tma.com.vn>
2020-06-08 16:58:56 +0700

    Refactor

--- js/test/run/index.js
+++ js/test/run/index.js
@@ -5,27 +5,27 @@ const path = require('path')
 const pkg = require('../../package')
 
 beforeEach (async function () {
-  const key = this.currentTest.title.split('-')[0]
-  console.log('key', this.currentTest)
+  const key = this.currentTest.title.split(':')[0]
+  this.currentTest.key = key
+  this.currentTest.tbs = []
+  
   const keyFile = `${key}.json`
-  this.currentTest.data = JSON.parse(fs.readFileSync(path.join('data', keyFile)))
-  if (fs.existsSync(path.join('init', keyFile))) {
-    const init = JSON.parse(fs.readFileSync(path.join('init', keyFile)))
+  const dataPath = path.join('data', keyFile)
+  const initPath = path.join('init', keyFile)
+
+  if (fs.existsSync(dataPath) && fs.existsSync(initPath)) {
+    this.currentTest.data = JSON.parse(fs.readFileSync(dataPath))
+    const init = JSON.parse(fs.readFileSync(initPath))
     this.currentTest.tbs = init.map((e) => e.table)
+    this.currentTest.tbs = [].concat(this.currentTest.data.input.table)
     return await Promise.all(init.map((e) => db.init(key, e.table, e.data)))
   }
-  this.currentTest.tbs = [].concat(this.currentTest.data.input.table)
 })
 
 afterEach (async function () {
-  if (!this.currentTest) return
-  return await Promise.all(this.currentTest.tbs.map((e) => db.empty(this.currentTest.title.split('-')[0], e)))
+  return await Promise.all(this.currentTest.tbs.map((e) => db.empty(this.currentTest.key, e)))
 })
 
-const go = (key, desc, handl) => {
-  describe(key, function() {
-    it(desc, handl)
-  })
-}
+const go = (key, desc, handl) => it(`${key}:${desc}`, handl)
 
 module.exports = { go, pkg }
--- js/test/tcs/1_ow.js
+++ js/test/tcs/1_ow.js
@@ -1,22 +1,25 @@
 const run = require('../run')
+const assert = require('assert')
 describe('Open Water', function() {
 
   run.go(
-    '# without API key',
-    'should return ow client with only one create-account function',
-    () => {
+    'tc_ow_1',
+    'OW without API key should return only one create-account function',
+    (done) => {
       var ow = run.pkg()
       assert.equal(Object.keys(ow).length, 1)
       assert.equal(Object.keys(ow.account).length, 1)
+      done()
     }
   )
 
   run.go(
-    '# with an API key',
-    'should return ow client with full funtions',
-    () => {
+    'tc_ow_2',
+    'OW with API key should return full functions',
+     (done) => {
       var ow = run.pkg('fakeAPIKey')
       assert.ok(Object.keys(ow).length > 1)
+      done()
     }
   )
 })
