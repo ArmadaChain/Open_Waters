94178a4d33dfd5ca183aa789a64bb5d72b028c98
dthanhoanh <dthanhoanh@tma.com.vn>
2020-06-09 15:14:45 +0700

    Test account should be changed by env

--- js/test/config/index.js
+++ js/test/config/index.js
@@ -1,14 +1,8 @@
 module.exports = {
-  log: {
-    dir: "logs",
-    filename: process.env.LOG_FILE_NAME || "events.log",
-    size: process.env.LOG_FILE_SIZE || 10485760,
-    level: process.env.LOG_LEVEL || "debug"
-  },
   acc: {
     normal: {
-      id: "CU0000003",
-      key: "VM4oeQcpt0SJcin1fi5v"
+      id: process.env.ACC_NORMAL_ID || "CU0000003",
+      key: process.env.ACC_NORMAL_KEY || "VM4oeQcpt0SJcin1fi5v"
     }
   }
 }
