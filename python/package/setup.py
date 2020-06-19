56a31989b0d0de9f18701a8d37dd5eb8001aa934
dthanhoanh <dthanhoanh@tma.com.vn>
2020-06-19 13:13:08 +0700

    python: configure setup.py

--- python/package/setup.py
+++ python/package/setup.py
@@ -1,13 +1,25 @@
-import setuptools
+from distutils.core import setup
 
-setuptools.setup(
-    name='open-waters',
-    version='1.0.0',
-    packages=setuptools.find_packages(),
-    url='',
-    license='',
-    author='Armadachain',
-    author_email='',
-    description='',
-    python_requires='>=3.6'
+setup(
+    name='open_waters',
+    packages=['open_waters'],
+    version='0.1',
+    license='ISC',
+    description='Open Waters allows simple integration of decentralized Hedera Hashgraph technology into existing '
+                'projects using the Armada Platform',
+    author='armada chain inc',
+    author_email='kent@armadachain.io',
+    url='https://github.com/ArmadaChain/Open_Waters/tree/master/python/package',
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',  # I explain this later on
+    keywords=['hederahashgraph'],
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Supply Chain :: Hederahashgraph:: Decentralization:: Dlt:: Blockchain',
+        'License :: ISC',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
